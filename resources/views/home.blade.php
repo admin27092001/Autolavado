ca896583c7102b075460bd785abb08a178d167da
Your Name <you@example.com>
2022-03-11 19:30:57 -0600

    Primer cambio

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('adminlte::page')
+
+
+@section('title', 'ADOLFO MELENDEZ')
+
+
+@section('content_header')
+    <h1>ADOLFO MELENDEZ</h1> 
+@stop
+
+@section('content')
+    <p>BIENVENIDO AL DASHBOARD</p>
+@stop
+
+
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
